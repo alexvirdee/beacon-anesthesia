0a7fd4d73ffb0452826ff49dbea5ede79e9caac1
Alex Virdee <alex.virdee08@gmail.com>
2023-07-12 20:08:45 -0400

    Unpublish gh-pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,6 @@ const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  pathPrefix: "/beacon-anesthesia",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
